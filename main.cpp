caedcd74c9b747a1d88f3bf0bfc01415312fb8b6
Alexandr Tristinskiy <3stan91@gmail.com>
2023-01-28 15:22:00 +0600

    add constant NUMBER

--- main.cpp
+++ main.cpp
@@ -5,19 +5,19 @@ int main() {
 
     std::cout << "Input owner's last name of apartments\n";
     std::cout << "--------------------------------------" << std::endl;
-    int n = 10;
-    std::string lastName[n];
+    const unsigned  int NUMBER = 10;
+    std::string lastName[NUMBER];
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < NUMBER; i++) {
         std::cout << "A last name " << i + 1 << " of owner\n";
         std::getline(std::cin, lastName[i]);
     }
     int apartmentNumber;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < NUMBER; i++) {
         std::cout << "Input # of apartment\n";
         std::cin >> apartmentNumber;
-        if (apartmentNumber < 1 || apartmentNumber > n) {
+        if (apartmentNumber < 1 || apartmentNumber > NUMBER) {
             std::cout << "Error. Invalid # of apartment\n";
             return 1;
         }
