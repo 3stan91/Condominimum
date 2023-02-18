0efc816e7cd1a7230c7ba012e805d6778eb91552
Alexandr Tristinskiy <3stan91@gmail.com>
2023-02-18 14:04:27 +0600

    initial

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main() {
     std::string lastName[NUMBER];
 
     for (int i = 0; i < NUMBER; i++) {
-        std::cout << "A last name " << i + 1 << " of owner\n";
+        std::cout << "A last name " << i + 1 << " of owner: \n";
         std::getline(std::cin, lastName[i]);
     }
     int apartmentNumber;
@@ -25,3 +25,5 @@ int main() {
     }
     return 0;
 }
+
+
